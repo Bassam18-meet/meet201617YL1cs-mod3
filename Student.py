ae579e9c090b44329f11fbc0ce766c8a0e2180c7
bassam18-meet <bassam2001.91@gmail.com>
2016-12-08 19:48:48 +0200

    girrafes

--- Student.py
+++ Student.py
@@ -7,4 +7,8 @@ class Student:
         self.ice_cream=ice_cream
     def print_summary(self):
         print("my name is "+self.name+" ,my age is "+self.age+" ,i live in "+self.hometown+" ,my height is "+self.height+" and my favorite ice cream flavor is "+self.ice_cream)
+
+    def get_giraffe_gap(self):
+        return 500-self.height
+    
         
